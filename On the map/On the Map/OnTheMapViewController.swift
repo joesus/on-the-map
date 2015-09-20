93aa686b87a36d0536fdf7f50d5bfd8564cd3a8a
Joe Susnick <jsusnick@itriagehealth.com>
2015-09-20 08:07:42 -0600

    pins added, are clickable

--- On the map/On the Map/OnTheMapViewController.swift
+++ On the map/On the Map/OnTheMapViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 import MapKit
 
-class OnTheMapViewController: UIViewController {
+class OnTheMapViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    var annotations = [MKPointAnnotation]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,7 @@ class OnTheMapViewController: UIViewController {
         var logoutButton: UIBarButtonItem = UIBarButtonItem(title: "Logout", style: UIBarButtonItemStyle.Plain, target: self, action: "logoutUser")
         self.navigationItem.backBarButtonItem = logoutButton
         
+        // Get student information
         getStudentInformationArray()
     }
 
@@ -28,11 +30,13 @@ class OnTheMapViewController: UIViewController {
         ParseAPIClient.sharedInstance().getStudentLocations() { (success, errorString, studentArray) in
             if success {
                 println("Successfully got students")
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.createAndAddPins(studentArray)
+                }
             } else {
                 self.displayError(errorString)
             }
         }
-
     }
     
     func displayError(errorString: String?) {
@@ -43,6 +47,26 @@ class OnTheMapViewController: UIViewController {
         })
     }
 
+    func createAndAddPins(studentArray: [StudentInformationStruct]?) {
+        // create pins
+        for student in studentArray! {
+            let lat = CLLocationDegrees(student.latitude as Double)
+            let long = CLLocationDegrees(student.longitude)
+            
+            // The lat and long are used to create a CLLocationCoordinates2D instance.
+            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            
+            var annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "\(student.firstName) \(student.lastName)"
+            annotation.subtitle = student.mediaURL
+            
+            // Finally we place the annotation in an array of annotations.
+            self.annotations.append(annotation)
+            self.mapView.addAnnotations(self.annotations)
+        }
+    }
+    
     @IBAction func logoutUser(sender: UIBarButtonItem) {
         // Logout if login was done with facebook
         let loginManager = FBSDKLoginManager()
@@ -54,5 +78,31 @@ class OnTheMapViewController: UIViewController {
     
     override func prefersStatusBarHidden() -> Bool {
         return true
+    }
+    
+    // MARK - MapView Delegate Methods
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinColor = .Red
+            pinView?.rightCalloutAccessoryView = UIButton.buttonWithType(.DetailDisclosure) as! UIButton
+        } else {
+            pinView?.annotation = annotation
+        }
+        return pinView
+    }
+    
+    func mapView(mapView: MKMapView!, annotationView view: MKAnnotationView!, calloutAccessoryControlTapped control: UIControl!) {
+        
+        if control == view.rightCalloutAccessoryView {
+            let app = UIApplication.sharedApplication()
+            app.openURL(NSURL(string: view.annotation.subtitle!)!)
+        }
     }
 }
