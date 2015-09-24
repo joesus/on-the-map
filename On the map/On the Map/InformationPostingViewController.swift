fdd970e4a19ef961123af06ef77b9c39f38af585
Joe Susnick <joesusnick@gmail.com>
2015-09-24 13:02:17 -0600

    added view to post student data

---
+++ On the map/On the Map/InformationPostingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InformationPostingViewController.swift
+//  On the Map
+//
+//  Created by A658308 on 9/24/15.
+//  Copyright (c) 2015 Joe. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class InformationPostingViewController: UIViewController {
+
+    @IBAction func didPressCancel(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
